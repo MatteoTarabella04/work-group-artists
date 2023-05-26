b0c7c44f4b6e2f50b96ddd068397cfad5a505da2
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-26 17:13:46 +0200

    add migration

---
+++ database/migrations/2023_05_26_150834_create_artists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('image', 200);
+            $table->string('genre', 50);
+            $table->date('birth_date');
+            $table->date('death_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artists');
+    }
+};

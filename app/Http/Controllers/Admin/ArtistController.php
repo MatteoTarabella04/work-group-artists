eddc4a73862694c5f55dde8cb6ad443d9c4f11cb
LeoneCarmelo <carmeloleone@outlook.it>
2023-06-06 16:30:14 +0200

    Add table + index.blade

--- app/Http/Controllers/Admin/ArtistController.php
+++ app/Http/Controllers/Admin/ArtistController.php
@@ -8,11 +8,6 @@ use App\Http\Controllers\Controller;
 
 class ArtistController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $artists = Artist::all();

0968ad98ffae268abff738b5015cb63a4f198539
fabiodelli <ntqdel@hotmail.com>
2023-06-12 16:29:09 +0200

    add relations between artist and albums

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Artist extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'image', 'genre', 'birth_date', 'death_date'];
+
+    
+    public function albums(): HasMany
+    {
+        return $this->hasMany(Album::class);
+    }
+
 }
+
+

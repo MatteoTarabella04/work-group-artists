13047191641134d47e38f7cdc6189b92436235f4
LeoneCarmelo <carmeloleone@outlook.it>
2023-06-12 18:06:35 +0200

    Add Model, + todo

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -11,7 +11,7 @@ class Album extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name','slug','cover','release_date','artist_id'];
+    protected $fillable = ['name','slug','cover','release_date','artist_id', 'genre_id'];
 
 public static function generateSlug($name)
     {

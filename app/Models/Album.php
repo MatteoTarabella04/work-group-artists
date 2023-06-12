d5cb4be47e555d2a0cfaec48b0b6371fb2f43ab0
fabiodelli <ntqdel@hotmail.com>
2023-06-12 16:05:18 +0200

    add model and migration

---
+++ app/Models/Album.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Album extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name','slug','cover','release_date','artist_id'];
+}

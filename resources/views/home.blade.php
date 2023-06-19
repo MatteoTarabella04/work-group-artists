0af50e9df6c3142de8249499ac3ad7a8af2a67c4
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-19 15:40:17 +0200

    add link and some style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,26 +4,37 @@
     <h1 class="text-center py-4">Artists:</h1>
 
     <div class="container">
+        <div class="my-2 text-center">
+            <a class="btn btn-dark" href="{{ route('artists.create') }}" role="button">Create new artist</a>
+        </div>
         <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
             @forelse ($artists as $artist)
                 <div class="col">
-                    <div class="card">
-                        <img class="card-img-top" src="{{ $artist->image }}" alt="Title">
-                        <div class="card-body">
-                            <h4 class="card-title">{{ $artist->name }}</h4>
-                            <ul>
-                                <li>
-                                    {{ $artist->genre }}
-                                </li>
-                                <li>
-                                    Birthdate: {{ $artist->birth_date }}
-                                </li>
-                                <li>
-                                    Death date: {{ $artist->death_date }}
-                                </li>
-                            </ul>
-                        </div>
+
+                    <div class="card artist">
+                        <a class="text-black" href="{{ route('artists.show', $artist->id) }}">
+                            <img class="card-img-top" src="{{ $artist->image }}" alt="Title">
+                            <div class="card-body">
+                                <h4 class="card-title">{{ $artist->name }}</h4>
+                                <ul>
+                                    <li>
+                                        {{ $artist->genre }}
+                                    </li>
+                                    <li>
+                                        Birthdate: {{ $artist->birth_date }}
+                                    </li>
+                                    <li>
+                                        Death date: {{ $artist->death_date }}
+                                    </li>
+                                    <li class="list-unstyled mt-2">
+                                        <a class="btn btn-dark" href="{{ route('artists.edit', $artist->id) }}"
+                                            role="button">Edit</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </a>
                     </div>
+
                 </div>
             @empty
                 <div class="col">
